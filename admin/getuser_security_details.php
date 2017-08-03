bc8b14159e1b7d0af5446df8c02fe467def0f39e
Patrick Python Book <pwalukagga@gmail.com>
2017-08-03 18:54:28 +0300

    security details for user

--- admin/getuser_security_details.php
+++ admin/getuser_security_details.php
@@ -1,12 +1,15 @@
-<?php require_once 'libs/database.php'; ?>
+<?php require_once '../libs/config.php'; ?>
+<?php require_once '../libs/database.php'; ?>
 <?php include 'includes/functions.php'; ?>
 
 <?php
 
+    $db = new database();
+
     $user = $_REQUEST['user'];
-    //$user = 'admin';
-    $security_details = $db->select_one("SELECT question, answer FROM user WHERE username = '". $user ."' ");
-    //print_r($security_details);
+    //$user = 'kenneth';
+    $security_details = $db->select_one("SELECT question, answer FROM users WHERE username = '". $user ."' ");
+    //print_r($security_details); die();
     header('Content-Type: application/json');
     echo json_encode($security_details);
     //print_r(json_encode($security_details));
